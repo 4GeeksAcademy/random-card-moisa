f97af3d3695a6e93488cf993048f8bc73eae327d
Moyunlimited <moy.estime@gmail.com>
2025-03-01 20:11:09 +0000

    done

--- src/app.js
+++ src/app.js
@@ -5,7 +5,56 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+document.querySelector("#newcardBtn").addEventListener("click", generateCard)
+  // generateCard ()
 };
+
+const generateCard = () => {
+  const iconDiv = document.querySelector(".top")
+  const iconEl =  document.querySelector(".bottom")
+  let icon = ["♦", "♥", "♠", "♣"]
+  let number = ["7", "9", "10", "J", "Q", "K", "A"]
+
+  let iconIndex = Math.floor(Math.random() * icon.length)
+  let numberIndex = Math.floor(Math.random() * number.length)
+  
+  let randomIcon = icon[iconIndex]
+  let randomNumber = number[numberIndex]
+
+  if(randomIcon == "♥") {
+    iconDiv.style.color = "red"
+  }
+
+else if(randomIcon == "♦") {
+   iconDiv.style.color = "red"
+}
+
+
+  else {
+    iconDiv.style.color = "black"
+  }
+
+  if(randomIcon == "♥") {
+    iconEl.style.color = "red"
+  }
+
+  else if (randomIcon == "♦") {
+    iconEl.style.color = "red"
+  }
+
+  else {
+    iconEl.style.color = "black"
+  }
+  
+
+
+
+
+  iconDiv.innerHTML = randomIcon
+  document.querySelector(".number").innerHTML = randomNumber
+  iconEl.innerHTML = randomIcon
+  
+}
+
+
